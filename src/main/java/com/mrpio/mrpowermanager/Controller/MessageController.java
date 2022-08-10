48d2a6bf51f5bc57555354b8db5df12f891fb069
Valerio Morelli <valeriomorelli50@gmail.com>
2022-08-10 13:24:47 +0200

    added socket online endpoint

--- src/main/java/com/mrpio/mrpowermanager/Controller/MessageController.java
+++ src/main/java/com/mrpio/mrpowermanager/Controller/MessageController.java
@@ -1,24 +1,27 @@
 package com.mrpio.mrpowermanager.Controller;
 
 import com.mrpio.mrpowermanager.Model.Command;
-import com.mrpio.mrpowermanager.Model.Message;
 import com.mrpio.mrpowermanager.Model.PcStatus;
 import com.mrpio.mrpowermanager.Model.UserStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
 public class MessageController {
+
+    final String ENDPOINT_SIGNUP = "/scheduleCommand/{token}/{pcName}";
+    final String ENDPOINT_UPDATE_PC_STATUS = "/updatePcStatus/{token}/{pcName}";
+    final String ENDPOINT_SET_ONLINE = "/setOnline/{token}/{pcName}";
+
+
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
@@ -31,41 +34,51 @@ public class MessageController {
         simpMessagingTemplate.convertAndSend("/topic/messages/" + to, message);
     }
 
-    //SCHEDULE COMMAND
-    @MessageMapping("/scheduleCommand/{token}/{pcName}")
+    @MessageMapping(ENDPOINT_SIGNUP)
     public void scheduleCommand(
             @DestinationVariable String token,
             @DestinationVariable String pcName,
             String args) throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
 //        System.out.println("/scheduleCommand/"+Controller.keepOnlyAlphaNum(token)+"/"+Controller.keepOnlyAlphaNum(pcName)+
 //                "\n"+ Arrays.toString(args.split("~")));
-        var newToken=Controller.keepOnlyAlphaNum(token);
-        var newPcName=Controller.keepOnlyAlphaNum(pcName);
-        var constructor=Command.class.getConstructor(String.class,String.class,String.class);
+        var newToken = Controller.keepOnlyAlphaNum(token);
+        var newPcName = Controller.keepOnlyAlphaNum(pcName);
+        var constructor = Command.class.getConstructor(String.class, String.class, String.class);
         Command command;
-        if(args.split("~").length==2)
-            command=constructor.newInstance(args.split("~")[0],args.split("~")[1],"null");
+        if (args.split("~").length == 2)
+            command = constructor.newInstance(args.split("~")[0], args.split("~")[1], "null");
         else
-            command=constructor.newInstance((Object[]) args.split("~"));
+            command = constructor.newInstance((Object[]) args.split("~"));
 
-        simpMessagingTemplate.convertAndSend("/server/"+newToken+"/"+newPcName+"/commands", command);
+        simpMessagingTemplate.convertAndSend("/server/" + newToken + "/" + newPcName + "/commands", command);
     }
 
-    @MessageMapping("/updatePcStatus/{token}/{pcName}")
+    @MessageMapping(ENDPOINT_UPDATE_PC_STATUS)
     public void updatePcStatus(
             @DestinationVariable String token,
             @DestinationVariable String pcName,
             String args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
 //        System.out.println("/updatePcStatus/"+Controller.keepOnlyAlphaNum(token)+"/"+Controller.keepOnlyAlphaNum(pcName)+
 //                "\n"+ Arrays.toString(args.split("~")));
-        var newToken=Controller.keepOnlyAlphaNum(token);
-        var newPcName=Controller.keepOnlyAlphaNum(pcName);
-        var constructor= Arrays.stream(PcStatus.class.getConstructors()).filter
-                        ((c) -> c.getParameterTypes().length > 1 &&c.getParameterTypes()[1]==String.class)
+        var newToken = Controller.keepOnlyAlphaNum(token);
+        var newPcName = Controller.keepOnlyAlphaNum(pcName);
+        var constructor = Arrays.stream(PcStatus.class.getConstructors()).filter
+                        ((c) -> c.getParameterTypes().length > 1 && c.getParameterTypes()[1] == String.class)
                 .collect(Collectors.toList()).get(0);
-        var pcStatus= (PcStatus) constructor.newInstance((Object[]) args.split("~"));
-        simpMessagingTemplate.convertAndSend("/client/"+newToken+"/"+newPcName+"/status", pcStatus);
+        var pcStatus = (PcStatus) constructor.newInstance((Object[]) args.split("~"));
+        simpMessagingTemplate.convertAndSend("/client/" + newToken + "/" + newPcName + "/status", pcStatus);
     }
+
+    @MessageMapping(ENDPOINT_SET_ONLINE)
+    public void setOnline(
+            @DestinationVariable String token,
+            @DestinationVariable String pcName,
+            String arg) {
+        var newToken = Controller.keepOnlyAlphaNum(token);
+        simpMessagingTemplate.convertAndSend("/client/" + newToken + "/online",
+                Map.of("pcName",pcName,"online",Boolean.valueOf(arg)));
+    }
+
 
     @MessageMapping("/keepAlive")
     public void blastToClientsHostReport() {
